4ff3211c9b1d62aef92a15fe3402e710c1c70803
sorshireality <tutunikssa@gmail.com>
2020-02-04 23:50:20 +0200

    establish WebSocket connection, trying to connect donationalerts API

--- tab/release.js
+++ tab/release.js
@@ -1,11 +1,43 @@
 jQuery(document).ready(function ($) {
+    //Create WebSocket connection
+
+    let socket = new WebSocket("wss://centrifugo.donationalerts.com/connection/websocket");
+    socket.onopen = function(e) {
+        socket.send(JSON.stringify({
+            "params": {
+                "token": "2H4SLFLKPE5TUjZuyHc3"
+            },
+            "id": 1
+        }));
+    };
+    socket.onmessage = function(event) {
+        alert(`[message] Data received from server: ${event.data}`);
+    };
+    console.log(socket);
+
+
+    $("#auth").on("click",function () {
+        $.ajax({
+            url: 'https://www.donationalerts.com/api/v1/user/oauth',
+            method: 'GET',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer 2H4SLFLKPE5TUjZuyHc3");
+            }, success: function(data){
+                alert(data);
+                console.log("ok");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('Fatal Error Creating The Request : ' + textStatus + ' | ' + errorThrown);
+            }
+        })
+    });
     $("#clickme").on("click",function ()
         {
             console.log("Trying to send request ....");
     $.ajax({
         url: 'https://www.donationalerts.com/api/v1/alerts/donations',
         beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer JpXlwd0NGIBWeUGuJr1x");
+            xhr.setRequestHeader("Authorization", "Bearer 2H4SLFLKPE5TUjZuyHc3");
         }, success: function(data){
            alert(data);
             console.log("ok");
