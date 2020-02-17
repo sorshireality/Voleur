4a9c073a4b292cdff835884cd155e916ef9f5f82
sorshireality <tutunikssa@gmail.com>
2020-02-17 14:33:55 +0200

    increase dellay

--- tab/requestes.php
+++ tab/requestes.php
@@ -3,7 +3,7 @@ include_once 'Helper.php';
 if (isset($_GET['code'])){
     $result = Helper::post('https://www.donationalerts.com/oauth/token',402,'nUFC2TP8nzx1BDHuV2N7qulZDvoAwz2vSh3fuf3T','https://voleur.000webhostapp.com/tab/requestes.php',$_GET['code']);
     if ($result[0] == 200) {
-    ?></body>: <script> setTimeout(function() { window.location = "token.php?token=<?php echo $result['access_token']; ?>"; }, 100); </script><?php
+    ?></body>: <script> setTimeout(function() { window.location = "token.php?token=<?php echo $result['access_token']; ?>"; }, 500); </script><?php
 } else {echo "Error while get aceess code : message -> "; print_r($result);exit;}
     }
 ?>
