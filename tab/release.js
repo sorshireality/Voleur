5d8a054cb8a3abca7c9217d10a61bac0b80bd01d
sorshireality <tutunikssa@gmail.com>
2020-02-17 16:32:12 +0200

    simple client - server requestes

--- tab/release.js
+++ tab/release.js
@@ -1,1 +1,24 @@
-console.log("шо интересного?");
+$(function() {
+    $('#button_token').click(function () {
+        var code = $('#input_field').val();
+        var url = 'https://voleur.000webhostapp.com/tab/token.php?code='+code;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url,true);
+        xhr.send(code);
+        xhr.onload = function() {
+            console.log(`Loaded: ${xhr.status}`);
+            console.log(JSON.parse(xhr.response));
+        };
+
+        xhr.onerror = function() { // происходит, только когда запрос совсем не получилось выполнить
+            console.log(`Error connect`);
+        };
+
+        xhr.onprogress = function(event) { // запускается периодически
+                                           // event.loaded - количество загруженных байт
+                                           // event.lengthComputable = равно true, если сервер присылает заголовок Content-Length
+                                           // event.total - количество байт всего (только если lengthComputable равно true)
+            console.log(`Loaded ${event.loaded} from ${event.total}`);
+        };
+    });
+});
--- tab/token.php
+++ tab/token.php
@@ -5,4 +5,9 @@ if (isset($_GET['token'])){
     $url = 'https://www.donationalerts.com/api/v1/alerts/donations';
     print_r(Helper::get($url,$token));
 }
+if (isset($_GET['code'])) {
+$arr["suc"] = $_GET;
+echo json_encode($arr);
+return true;
+} else return false;
 ?>
