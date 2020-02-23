25800b71942203fabbe156c29cbe4ab434518b3d
sorshireality <tutunikssa@gmail.com>
2020-02-23 23:05:35 +0200

    remove server folder at all, setup app with new link /api

--- tab/release.js
+++ tab/release.js
@@ -1,17 +1,28 @@
 $(function() {
     $('#create_token_button').click(function () {
-        var url = 'https://www.donationalerts.com/oauth/authorize?response_type=code&client_id=402&redirect_uri=https://voleur.000webhostapp.com/tab/server/processor.php&%20%20%20%20scope=oauth-donation-index';
-        window.open(url);
-    });
-    $('#button_token').click(function () {
-        var code = $('#input_field').val();
-        var url = 'https://voleur.000webhostapp.com/tab/server/catch.php?code='+code;
+        var url = 'https://www.donationalerts.com/oauth/authorize?response_type=code&client_id=402&redirect_uri=https://voleur.000webhostapp.com/api/processor.php&%20%20%20%20scope=oauth-donation-index';
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url,true);
-        xhr.send(code);
+        xhr.send('authorize');
         xhr.onload = function() {
-            console.log(`Loaded: ${xhr.status}`);
-            console.log(JSON.parse(xhr.response));
+            console.log('Loaded: '+xhr.status);
+            if (xhr.status == 200) {
+                alert('Токен успешно создан!');
+            }
+        }
+        xhr.onerror = function() { // происходит, только когда запрос совсем не получилось выполнить
+            console.log(`Error connect`);
+        }
+    });
+    $('#button_token').click(function () {              //обрабатываем нажатие на кнопку
+        var user_key = $('#input_field').val();
+        var url = 'https://voleur.000webhostapp.com/api/catch.php?code='+user_key;  
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url,true);
+        xhr.send(user_key);
+        xhr.onload = function() { //получаем ответ от сервера
+            console.log(`Loaded: ${xhr.status}`); //статус
+            console.log(JSON.parse(xhr.response)); //тело ответа
         };
 
         xhr.onerror = function() { // происходит, только когда запрос совсем не получилось выполнить
--- tab/server/processor.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "Helper.php";
-if (isset($_GET['code'])) {
-    Helper::getcode($_GET['code']);
-}
-if (isset($_GET['token'])){
-    Helper::gettoken($_GET['token']);
-}
