bc3616df0b8aa33aca996d3a5c9f9d9f866ce704
eternaltoxic <spkespke@gmail.com>
2020-02-25 19:34:16 +0200

    file session.js redirect to table(SASHA HELP)

--- tab/session.js
+++ tab/session.js
@@ -1,8 +1,10 @@
-var field = document.getElementById("input_field");
-$('#button_token').click(function () {
-    localStorage.setItem('token', field.value);
-    if(localStorage.length>0) {
-        document.location.href = "Voleur/tab/table/table.html";
+$(function() {
+
+    var field = document.getElementById("input_field");
+    $('#button_token').click(function () {
+        localStorage.setItem('token', field.value);
+    if(localStorage.length > 0){
+        document.location.href = "table/table.html";
     }
-    console.log(localStorage.getItem("token"));
+    });
 });
