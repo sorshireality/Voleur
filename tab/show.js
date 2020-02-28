a9763441310e2fccca4bdaecb0b60f5aa5ff8fb2
eternaltoxic <spkespke@gmail.com>
2020-02-28 19:22:05 +0200

    another fix

--- tab/show.js
+++ tab/show.js
@@ -3,7 +3,7 @@ $(function() {
         $('.additional').animate({right: '270px'});
         $('.additional').css('display','block');
     });
-    $('#create_uuid_button').on('click', function() {
+    $('#create_token_button').on('click', function() {
 
         $('.uuid_code').animate({right: '95px'});
         $('.uuid_code').css('display','block');
