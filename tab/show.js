695e920c97515700e24b082247b6bc47431f3351
eternaltoxic <spkespke@gmail.com>
2020-02-28 19:15:33 +0200

    add animation

--- tab/show.js
+++ tab/show.js
@@ -1,7 +1,12 @@
 $(function() {
     $('#show_button').click(function () {
-        $('#refresh,#remove').css('display','block');
-    });$('#create_token_button').click(function () {
+        $('.additional').animate({right: '270px'});
+        $('.additional').css('display','block');
+    });
+    $('#create_uuid_button').on('click', function() {
+
+        $('.uuid_code').animate({right: '95px'});
+        $('.uuid_code').css('display','block');
 
 
     });
