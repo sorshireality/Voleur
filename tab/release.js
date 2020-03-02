79314de8dcaff342cccfaa0558e58c9dfe44801d
sorshireality <tutunikssa@gmail.com>
2020-03-02 18:07:21 +0200

    add windows remover

--- tab/release.js
+++ tab/release.js
@@ -1,11 +1,12 @@
 $(function() {
     $('#create_token_button').click(function () {
         url = 'https://www.donationalerts.com/oauth/authorize?response_type=code&client_id=402&redirect_uri=https://voleur.000webhostapp.com/api/processor.php&%20%20%20%20scope=oauth-user-show oauth-donation-subscribe oauth-donation-index';
-
-        chrome.windows.create({
+        let auth_id = 0;
+        var tab_id = chrome.windows.create({
             url: url,
             type: "popup"
         }, function(win) {
+            auth_id = win['id'];
         });
 
         var xhr = new XMLHttpRequest();
@@ -15,6 +16,7 @@ $(function() {
             console.log('Loaded: '+xhr.status);
             if (xhr.status == 200) {
                 console.log(xhr.response);
+                chrome.windows.remove(auth_id);
             } else {
                 console.log("while create token we got a error: "+xhr.status);
             }
