ed16eeafdb0656fd11c167bb254a475e25876613
sorshireality <tutunikssa@gmail.com>
2020-03-09 17:03:10 +0200

    get donats by user key

--- tab/release.js
+++ tab/release.js
@@ -13,16 +13,25 @@ $(function() {
             auth_id = win['id'];
         });
     });
-      $('#button_token').click(function () {              //обрабатываем нажатие на кнопку
-        var user_key = $('#input_field').val();
-        var url = 'https://voleur.000webhostapp.com/api/catch.php?code='+user_key;  
+      $('#enter_uuid_field').on('input', function(){  //обрабатываем нажатие на кнопку
+          var user_key = $('#enter_uuid_field').val();
+          if (user_key!=""){
+              document.getElementById('loader').style.display = "block";
+          document.getElementById('enter_uuid_field').style.border = "3px solid black"; //
+
+        var url = 'https://voleur.000webhostapp.com/api/catch.php?code='+user_key;
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url,true);
-        xhr.send(user_key);
-        xhr.onload = function() { //получаем ответ от сервера
+        xhr.send();
+        xhr.onload = function() {//получаем ответ от сервера
+            document.getElementById('loader').style.display = "none";
             console.log(`Loaded: ${xhr.status}`); //статус
-            console.log(JSON.parse(xhr.response)); //тело ответа
-
+            response =JSON.parse(xhr.response); //тело ответа
+            console.log(response);
+            if (response["result"] == 'error') {document.getElementById('enter_uuid_field').style.border = "3px solid red";} else
+                 {document.getElementById('enter_uuid_field').style.border = "3px solid lightgreen"; setTimeout(function () {
+                    console.log('redirect there'); //redirect
+                },1500);}
         };
 
         xhr.onerror = function() { // происходит, только когда запрос совсем не получилось выполнить
@@ -35,5 +44,7 @@ $(function() {
                                            // event.total - количество байт всего (только если lengthComputable равно true)
             console.log(`Loaded ${event.loaded} from ${event.total}`);
         };
+          }
     });
-});
+      });
+
